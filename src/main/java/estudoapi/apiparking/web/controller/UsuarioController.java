c30fd97b589e8f0edf5ed83cdcb05fb005335ef8
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-19 14:34:23 -0300

    criado método para buscar usuário por id.

--- src/main/java/estudoapi/apiparking/web/controller/UsuarioController.java
+++ src/main/java/estudoapi/apiparking/web/controller/UsuarioController.java
@@ -5,10 +5,8 @@ import estudoapi.apiparking.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -21,4 +19,11 @@ public class UsuarioController {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario>finduser(@PathVariable Long id) {
+        Usuario user = usuarioService.buscar(id);
+        return ResponseEntity.ok(user);
+    }
+
 }
