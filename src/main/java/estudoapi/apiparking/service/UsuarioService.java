6ecdd61344ad27a72680814cec540171982a05f9
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-19 08:40:39 -0300

    criado metodo create usuario

--- src/main/java/estudoapi/apiparking/service/UsuarioService.java
+++ src/main/java/estudoapi/apiparking/service/UsuarioService.java
@@ -1,11 +1,18 @@
 package estudoapi.apiparking.service;
 
+import estudoapi.apiparking.entity.Usuario;
 import estudoapi.apiparking.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/estudoapi/apiparking/web/controller/UsuarioController.java
+++ src/main/java/estudoapi/apiparking/web/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package estudoapi.apiparking.web.controller;
 
+import estudoapi.apiparking.entity.Usuario;
 import estudoapi.apiparking.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
@@ -10,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario>create(Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
