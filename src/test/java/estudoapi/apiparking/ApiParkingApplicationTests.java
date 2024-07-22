557674aa598e84f107eba0b0d6b94987b6610c8a
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-22 12:35:53 -0300

    Revert "exclusão de classe teste"
    This reverts commit b9e8d6bdec5d712b1b75eb520f108d99b94a1833.

---
+++ src/test/java/estudoapi/apiparking/ApiParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package estudoapi.apiparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
