095002a0c2fb031e09f48a44430c9ad49a9c7b71
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-23 11:36:28 -0300

    fix: correção de scripts

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,2 +1,2 @@
-insert into USUARIOS (id, username, password, role) values(100, 'mariaz@gamil.com', 'ROLE_ADMIN'),
-(200, 'joaoz@gmail.com', '654321', 'ROLE_CLIENTE'), (300, 'ze@gmail.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values
+(100, 'mariaz@gamil.com','123456' ,'ROLE_ADMIN'),(200, 'joaoz@gmail.com', '654321', 'ROLE_CLIENTE'), (300, 'ze@gmail.com', '123456', 'ROLE_CLIENTE');
