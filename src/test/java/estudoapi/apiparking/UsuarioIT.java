bda2c5bfd732b32489839cf256f956595dd4a250
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-23 18:53:57 -0300

    testes implementados
    -teste para listar usuario;
    -teste para buscar usuario;
    -teste para trocar a senha;

--- src/test/java/estudoapi/apiparking/UsuarioIT.java
+++ src/test/java/estudoapi/apiparking/UsuarioIT.java
@@ -2,6 +2,7 @@ package estudoapi.apiparking;
 
 import estudoapi.apiparking.web.dto.UsuarioCreateDto;
 import estudoapi.apiparking.web.dto.UsuarioResponseDto;
+import estudoapi.apiparking.web.dto.UsuarioSenhaDto;
 import estudoapi.apiparking.web.exception.ErrorMensage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -136,4 +142,120 @@ public class UsuarioIT {
 
 
     }
+    @Test
+    public  void findUser_withIdexists_returUsuarioREspondeDtoWithstatus200(){
+        UsuarioResponseDto responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios/100")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isEqualTo(100);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("mariaz@gamil.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("ADMIN");
+    }
+    @Test
+    public  void findUser_withIdUnxists_returErrorMensagetoWithstatus404(){
+        ErrorMensage responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios/500")
+                .exchange()
+                .expectStatus().isEqualTo(404)
+                .expectBody(ErrorMensage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
+
+    }
+    @Test
+    public  void UpdatePassword_withusernameAndPasswordValids_returnUserWithstatus200(){
+        UsuarioResponseDto responseBody = testClient
+                .patch()
+                .uri("/api/v1/usuarios/200")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioSenhaDto("654321", "654321", "654321"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isEqualTo(200);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("joaoz@gmail.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+    }
+    @Test
+    public  void UpdatePassword_withPasswordUnvalids_returnErrorMensageWithstatus400(){
+        ErrorMensage responseBody = testClient
+                .patch()
+                .uri("/api/v1/usuarios/100")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioSenhaDto("654321", "654321", "654321"))
+                .exchange()
+                .expectStatus().isEqualTo(400)
+                .expectBody(ErrorMensage.class)
+                .returnResult().getResponseBody();
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
+
+        responseBody = testClient
+                .patch()
+                .uri("/api/v1/usuarios/100")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioSenhaDto("123456", "654323", "654321"))
+                .exchange()
+                .expectStatus().isEqualTo(400)
+                .expectBody(ErrorMensage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
+    }
+    @Test
+    public  void UpdatePassword_withidNotExists_returnErrorMensageWithstatus404() {
+        ErrorMensage responseBody = testClient
+                .patch()
+                .uri("/api/v1/usuarios/500")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioSenhaDto("654321", "654321", "654321"))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ErrorMensage.class)
+                .returnResult().getResponseBody();
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
+
+    }
+
+    @Test
+    public void listUser_returnListWithStatus200() {
+        List<UsuarioResponseDto> responseBody = testClient
+                .get()
+                .uri("/api/v1/usuarios")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(UsuarioResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.size()).isGreaterThan(0);
+        assertThat(responseBody.size()).isEqualTo(3);
+    }
+    @Test
+    @Sql(scripts = "classpath:sql/usuarios/usuarios-delete.sql")
+    public void ListUser_returnEmptyListWithStatus200() {
+        testClient
+                .get()
+                .uri("/api/v1/usuarios")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(UsuarioResponseDto.class)
+                .hasSize(0);
+    }
+
 }
