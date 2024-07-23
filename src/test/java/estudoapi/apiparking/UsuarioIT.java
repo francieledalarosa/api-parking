60aa6be437d72a9bd3e210d13f08f53522ae16af
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-23 09:29:37 -0300

    configuração na classe teste
    -adição de biblioteca webflux no pom;
    - criação de arquivos sql;
    -criação da classe UsuarioIT para teste;

---
+++ src/test/java/estudoapi/apiparking/UsuarioIT.java
@@ -0,0 +1,14 @@
+package estudoapi.apiparking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient testClient;
+}
