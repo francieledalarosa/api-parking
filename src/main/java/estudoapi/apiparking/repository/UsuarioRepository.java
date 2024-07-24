b25f9a8da319d1c31ef940494b018e096e6e9e76
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-24 18:48:39 -0300

    Update UsuarioRepository.java

--- src/main/java/estudoapi/apiparking/repository/UsuarioRepository.java
+++ src/main/java/estudoapi/apiparking/repository/UsuarioRepository.java
@@ -2,6 +2,12 @@ package estudoapi.apiparking.repository;
 
 import estudoapi.apiparking.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+    @Query("select u.role from Usuario u where u.username like: username")
+    Usuario.Role findRoleByUsername(String username);
 }
