7f95b6fe65229acf24792c8a0404cecac692484b
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-26 09:19:54 -0300

    mensagem de erro 401 e 403

--- src/main/java/estudoapi/apiparking/config/SpringSecurityConfig.java
+++ src/main/java/estudoapi/apiparking/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package estudoapi.apiparking.config;
 
+import estudoapi.apiparking.jwt.JwtAuthenticationEntryPoint;
 import estudoapi.apiparking.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +34,8 @@ public class SpringSecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).addFilterBefore(
                         jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
+                ).exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                 ).build();
     }
     @Bean
--- src/main/java/estudoapi/apiparking/web/exception/ApiExceptionHandler.java
+++ src/main/java/estudoapi/apiparking/web/exception/ApiExceptionHandler.java
@@ -13,19 +13,32 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.access.AccessDeniedException;
+
+
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorMensage> accessDeniedException(AccessDeniedException ex, HttpServletRequest request){
+        log.error("Api error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMensage(request, HttpStatus.FORBIDDEN,ex.getMessage()));
+        
+    }
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorMensage> entityNotFoundException(EntityNotFoundException ex,
-                                                                        HttpServletRequest request){
+                                                                HttpServletRequest request){
         log.error("Api error - ", ex);
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorMensage(request, HttpStatus.NOT_FOUND,ex.getMessage()));
-        
+
     }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMensage> methodArgumentNotValidException(MethodArgumentNotValidException ex,
                                                                         HttpServletRequest request,
