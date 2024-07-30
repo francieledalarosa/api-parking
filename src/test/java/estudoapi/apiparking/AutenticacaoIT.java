958874f39418e7908528155a5600233fa0b6aa96
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-30 19:25:04 -0300

    Create AutenticacaoIT.java
    -teste com autenticação válida;

---
+++ src/test/java/estudoapi/apiparking/AutenticacaoIT.java
@@ -0,0 +1,34 @@
+package estudoapi.apiparking;
+
+import estudoapi.apiparking.jwt.JwtToken;
+import estudoapi.apiparking.web.dto.UsuarioLoginDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AutenticacaoIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void autenticar_ComCredenciaisValidas_REtornarTokenComStatus200() {
+        JwtToken responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("mariaz@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    }
+}
