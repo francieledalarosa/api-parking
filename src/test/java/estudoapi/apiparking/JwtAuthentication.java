cd6a680c83c4d59f6ad72b4d6f0da62d72dc52c5
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-30 14:20:30 -0300

    Update JwtAuthentication.java

--- src/test/java/estudoapi/apiparking/JwtAuthentication.java
+++ src/test/java/estudoapi/apiparking/JwtAuthentication.java
@@ -5,19 +5,20 @@ import estudoapi.apiparking.web.dto.UsuarioLoginDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class JwtAuthentication {
 
     public static Consumer<HttpHeaders> getHeaderAuthorization(WebTestClient client, String username, String password) {
-        String token = client
+        String token = Objects.requireNonNull(client
                 .post()
-                .uri("/api/vq/auth")
+                .uri("/api/v1/auth")
                 .bodyValue(new UsuarioLoginDto(username, password))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(JwtToken.class)
-                .returnResult().getResponseBody().getToken();
+                .returnResult().getResponseBody()).getToken();
         return headers -> headers.add(HttpHeaders.AUTHORIZATION, "Bearer" + token);
     }
 }
