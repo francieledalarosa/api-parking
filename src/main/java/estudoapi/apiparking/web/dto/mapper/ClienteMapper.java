ac82841818075900aa87934b00ba6767d4bf767d
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-07-30 22:48:53 -0300

    Create ClienteMapper.java

---
+++ src/main/java/estudoapi/apiparking/web/dto/mapper/ClienteMapper.java
@@ -0,0 +1,19 @@
+package estudoapi.apiparking.web.dto.mapper;
+
+import estudoapi.apiparking.entity.Cliente;
+import estudoapi.apiparking.web.dto.ClienteCreateDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClienteMapper {
+
+    public static Cliente tocliente(ClienteCreateDto dto) {
+        return new ModelMapper().map(dto, Cliente.class);
+    }
+
+    public static ClienteResponseDto toDto(Cliente cliente) {
+        return new ModelMapper().map(cliente, ClienteResponseDto.class);
+    }
+}
