6ec4c398c2b47fb74671dfd3c878d36c82409ba3
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-08-04 18:46:45 -0300

    impementação de metodos do estacionamento

--- src/main/java/estudoapi/apiparking/service/ClienteVagaService.java
+++ src/main/java/estudoapi/apiparking/service/ClienteVagaService.java
@@ -1,8 +1,12 @@
 package estudoapi.apiparking.service;
 
 import estudoapi.apiparking.entity.ClienteVaga;
+import estudoapi.apiparking.exception.EntityNotFoundException;
 import estudoapi.apiparking.repository.ClienteVagasRepository;
+import estudoapi.apiparking.repository.projection.ClienteVagaProjection;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,5 +19,29 @@ public class ClienteVagaService {
     @Transactional
     public ClienteVaga salvar(ClienteVaga clienteVaga) {
         return repository.save(clienteVaga);
+    }
+
+    @Transactional(readOnly = true)
+    public ClienteVaga buscarPorRecibo(String recibo) {
+        return repository.findByReciboAndDataSaidaIsNull(recibo).orElseThrow(
+                () -> new EntityNotFoundException(
+                        String.format("Recibo '%s' não encontrado no sistema ou check-out já realizado", recibo)
+                )
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public long getTotalDeVezesEstacionamentoCompleto(String cpf) {
+        return repository.countByClienteCpfAndDataSaidaIsNotNull(cpf);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClienteVagaProjection> buscarTodosPorClienteCpf(String cpf, org.springframework.data.domain.Pageable pageable) {
+        return repository.findAllByClienteCpf(cpf, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClienteVagaProjection> buscarTodosPorUsuarioId(Long id, Pageable pageable) {
+        return repository.findAllByClienteUsuarioId(id, pageable);
     }
 }
